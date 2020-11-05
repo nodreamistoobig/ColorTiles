3db0936c82163ce391bda3cec981973bab52ef4a
Alenafox <57696470+Alenafox@users.noreply.github.com>
2020-11-05 22:32:13 +0800

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.colortiles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
